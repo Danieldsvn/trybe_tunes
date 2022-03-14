9fc676a86a67e8cd75c110b6c1967a68ce014b5c
Daniel Saraiva <danieldsvn@gmail.com>
2022-03-13 22:39:42 -0300

    requisito 2 em andamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disable: true,
+      input: '',
+    }
+  }
+
+  handleInput = (event) => {
+    const { event } = this
+    this.setState((event) => {
+
+    })
+  }
   render() {
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input onChange={this.handleInput(event)} data-testid="login-name-input" type="text" />
+        <button type="submit" disabled data-testid="login-submit-button">Entrar</button>
+
+      </div>
+
     );
   }
 }
