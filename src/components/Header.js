d516e44a5ff724cb928b8d55ff0131a4d474d39f
Daniel Saraiva <danieldsvn@gmail.com>
2022-03-15 02:26:05 -0300

    Requisito 2, 3 e 4 em andamento

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div data-testid="header-component">
+        <Link data-testid="link-to-search"  to="/search">
+          <p>Pesquisa</p>
+        </Link>
+        <Link data-testid="link-to-favorites"  to="./favorites">
+          <p>Músicas favoritas</p>
+        </Link>
+        <Link data-testid="link-to-profile"  to="/profile">
+          <p>Perfil</p>
+        </Link>
+      </div>
+    );
+  }
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      disable: true,
       input: '',
-    }
+    };
   }
 
   handleInput = (event) => {
-    const { event } = this
-    this.setState((event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
 
-    })
-  }
   render() {
+    const { input } = this.state;
+    const minInput = 3;
     return (
-      <div data-testid="page-login">
-        <input onChange={this.handleInput(event)} data-testid="login-name-input" type="text" />
-        <button type="submit" disabled data-testid="login-submit-button">Entrar</button>
+      <form onSubmit={ createUser({ name: input }) } data-testid="page-login">
+        <input
+          onChange={ this.handleInput }
+          value={ input }
+          data-testid="login-name-input"
+          type="text"
+        />
+        <button
+          type="submit"
+          disabled={ input.length < minInput }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
 
-      </div>
+      </form>
 
     );
   }
