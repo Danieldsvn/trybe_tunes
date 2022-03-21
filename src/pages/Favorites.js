20dddf89c9e348980f332fbccfc2474615e07179
Daniel Saraiva <danieldsvn@gmail.com>
2022-03-21 03:29:54 -0300

    requisito 11 em andamento

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import AlbumCard from '../components/AlbumCard';
 
 export default class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoadingFavorite: false,
+      favoritesSongs: [],
+    };
+  }
+
+  // componentDidMount() {
+  //   handleGetFavorites()
+  // }
   render() {
+    const { isLoadingFavorite, favoritesSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        { isLoadingFavorite ? <Loading /> : favoritesSongs.map((song) => (
+          <AlbumCard
+            key={ song.trackId }
+            trackId={ song.trackId }
+            handleFavorite={ handleSaveFavorites() }
+            trackName={ song.trackName }
+            previewUrl={ song.previewUrl }
+          />
+        ))}
       </div>
     );
   }
